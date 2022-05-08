972eb2150aa8625ac9c2b14d1dd23e8550e6c566
kenmoh <kenneth.aremoh@gmail.com>
2022-05-08 12:13:17 +0100

    added a custom filter(FilterInventory) field

--- store/admin.py
+++ store/admin.py
@@ -4,6 +4,22 @@ from django.utils.html import format_html, urlencode
 from django.urls import reverse
 from .models import Collection, Product, Customer, Order
 
+product_is_low = 'low_inventory'
+
+
+class FilterInventory(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            (product_is_low, 'Low')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == product_is_low:
+            return queryset.filter(inventory__lt=20)
+
 
 # noinspection PyMethodMayBeStatic
 @admin.register(Product)
@@ -11,11 +27,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 50
+    list_filter = ['collection', 'last_update', FilterInventory]
 
     # Computed Field
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
-        if product.inventory < 10:
+        if product.inventory < 20:
             return 'Low'
         return 'OK'
 
@@ -27,9 +44,16 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 50
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
+    @admin.display(ordering='orders')
     def orders(self, customer):
         return customer.order_set.count()
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders=Count('order')
+        )
 
 
 # noinspection PyMethodMayBeStatic
