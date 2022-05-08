9124f317a1d56e1eed4e25cda249de64328d62e5
kenmoh <kenneth.aremoh@gmail.com>
2022-05-08 17:57:01 +0100

    added a function to calculate tax

--- store/serializers.py
+++ store/serializers.py
@@ -1,8 +1,14 @@
+from decimal import Decimal
 from rest_framework import serializers
+from .models import Product
 
 
-# noinspection PyAbstractClass
+# noinspection PyAbstractClass,PyMethodMayBeStatic
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     unit_price = serializers.DecimalField(max_digits=8, decimal_places=2)
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.05)
